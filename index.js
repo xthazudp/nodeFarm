383bcc6c773e954e39555ae8d8be6528adde4691
xthazudp <xthazudp@gmail.com>
2020-08-17 13:53:01 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,134 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
+
+
+///////////////////////////////
+// FILES
+///////////////////////////////
+
+/// Blocking, SYNC Method ///
+
+/* 
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('file written!!');
+*/
+
+
+/// Non-blocking, ASYNC Method ///
+
+/* 
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('ERROR!!');
+
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('File written!!!');
+            });
+        });
+    });
+});
+
+console.log('Will read file soon!!!');
+*/
+
+///////////////////////////////////
+// SERVER
+//////////////////////////////////
+
+/// Creating A Server ///
+
+/* 
+const server = http.createServer((req, res) => {
+   res.end('Hello from the server!!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listining the request on port 80000');
+});
+*/
+
+/// Routing ///
+
+/* 
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+
+    const pathName = req.url;
+
+    if (pathName === '/overview' || pathName === '/') {
+        res.end('This is overview.');
+    } else if (pathName === '/product') {
+        res.end('This is product.');
+    } else{
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found</h1>');
+    }
+    res.end('Hello from the server!!');
+});
+ 
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listining the request on port 80000');
+}); 
+*/
+
+/// Building a simple API
+
+const tempOverview =  fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard =  fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct =  fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data =  fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+
+    const { query, pathname } = url.parse(req.url, true);
+    
+
+    // Overview
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
+
+    // Products 
+    } else if (pathname === '/product') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
+
+    // API
+    } else if (pathname === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+
+    // Not Found
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found</h1>');
+    }
+    // res.end('Hello from the server!!');
+});
+ 
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listining the request on port 80000');
+}); 
